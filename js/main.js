cad55d77b345b037ed768c3a662b236dc94de34e
=crycetruly <=crycetruly@gmail.com>
2018-08-14 10:49:29 +0300

    make it better'

---
+++ js/main.js
@@ -0,0 +1,34 @@
+let countdown=document.querySelector('.countdown');
+const lauchdate=new Date('Aug 24, 2018 13:00:00').getTime();
+var intvl=setInterval(()=>{
+//get today
+
+var now=new Date().getTime();
+
+//from now to launch
+let dist=lauchdate-now;
+let days=Math.floor(dist/(1000*60*60*24));
+console.log(days);
+let hours=Math.floor(dist%((1000*60*60*24))/(1000*60*60));
+
+const mins=Math.floor(dist%((1000*60*60))/(1000*60));
+const secs=Math.floor(dist%((1000*60))/(1000));
+
+//display result
+
+countdown.innerHTML=    `
+
+<div>${days} <span>Days</span></div>
+<div>${hours} <span>Hours</span></div>
+<div>${mins} <span>Minutes</span></div>
+<div>${secs} <span>Seconds</span></div>
+
+`;
+if(dist<0){
+    clearInterval(intvl);
+    countdown.style.color='#12a2b8';
+    document.querySelector('.comingsoon').innerHTML='Happy Hour';
+    countdown.innerHTML='Launched';
+}
+
+},1000);
